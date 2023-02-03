de62dea3f58be5ccfce14b8f618355b63e5342e4
ajkn1904 <ajknishat@gmail.com>
2023-02-03 23:05:27 +0600

    firebase added

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const Home = () => {
+   
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
